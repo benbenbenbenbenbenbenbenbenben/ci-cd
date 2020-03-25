7a6ad1f91d9084289e3052e3a9f96c9760d2429a
Ben Kersnovske <ben.kersnovske@Bens-MacBook-Pro-50893.local>
2020-03-25 15:31:46 +0000

    Add unit tests for calculator

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-print('Hello World')
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
---
+++ test.py
@@ -0,0 +1,12 @@
+"""
+Unit tests
+"""
+import app
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == app.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == app.subtract(4, 2)
