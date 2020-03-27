9de19eb6ebc7d4c48891ed725cbdffe916980b23
Ben Kersnovske <ben.kersnovske@Bens-MacBook-Pro-50893.local>
2020-03-27 12:42:06 +0000

    updated

--- test_app.py
+++ test_app.py
@@ -1,12 +1,10 @@
 """
-Unit tests
+Unit tests using pytest
 """
 import app
 
-class TestCalculator:
+def test_addition():
+    assert 4 == app.add(2, 2)
 
-    def test_addition(self):
-        assert 4 == app.add(2, 2)
-
-    def test_subtraction(self):
-        assert 2 == app.subtract(4, 2)
+def test_subtraction():
+    assert 2 == app.subtract(4, 2)
